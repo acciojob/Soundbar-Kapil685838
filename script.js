b2f29fad69d7d43f90e1e2a91a3a4eeb0cf1b273
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-05-07 15:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-//your JS code here. If required.
-const audio = document.getElementById('audio');
-
-function play(src){
-	audio.src = src;
-	audio.play();
-}
-
-function stop(){
-	audio.pause();
-}
+function play() { 
+     var audio = document.getElementById("audio"); 
+     audio.play(); 
+   } 
+ function play2(){ 
+         var audio = document.getElementById("audio2"); 
+     audio.play2(); 
+ }
